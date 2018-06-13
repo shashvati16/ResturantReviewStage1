1e85f2c11d0c96d3f7ab15129dc8029ad40a50b0
Shashvati Dash <shashvati.dash@gmail.com>
2018-06-12 22:33:04 -0400

    fix: fixed service worker code

--- js/sw.js
+++ js/sw.js
@@ -1,43 +1,38 @@
 var webCache = 'mws-static-001';
-self.adddEventListener("install", event => {
+var urlsToCache = ["/",
+"/index.html",
+"/restaurant.html",
+"/js/",                
+"/img/error.png",
+"/css/styles.css",
+"/data/restaurants.json"];
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(webCache).then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/restaurant.html",
-                "/js/",
-                "/js/main.js",
-                "/js/dbhelper.js",
-                "/js/restaurant_info.js",
-                "/js/register.js",
-                "/img/error.png",
-                "/css/styles.css",
-                "/data/restaurants.json"
-            ]).catch(error => {
-                console.log("Caches open failed: " + error);
-            });
-        })
-    );
-});
+        caches.open(webCache).then(function(cache) {
+            return cache.addAll(urlsToCache);
+          })
+        );
+    });    
+      
+  
 
   
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   let cacheRequest = event.request;
   let cacheObj = new URL(event.request.url);  
-  if(event.request.url.indexOf("/restaurant.html") > -1){
+  /*if(event.request.url.indexOf("/restaurant.html") > -1){
     const cacheURL = "restaurant.html";
     cacheRequest = new Request(cacheURL);
   }
   if (cacheObj.hostname !== "localhost") {
     event.request.mode = "no-cors";
-  }
+  }*/
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function(response) {
       return response || 
       fetch(event.request)
-        .then (eventresponse  => {
-          return caches.open(staticCacheName).then(cache => {
+        .then (function (eventresponse) {
+          return caches.open(webCache).then(function(cache) {
             cache.put(event.request, eventresponse.clone());
             return eventresponse;
           });
